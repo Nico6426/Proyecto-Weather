78e9f7438b5a4b1febc448bb48259eaef258b48a
Nico <dececconicolas@gmail.com>
2021-09-23 20:59:45 -0300

    Url for environment.prod

--- WeatherFE/src/environments/environment.prod.ts
+++ WeatherFE/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://localhost:44315/',
+  getURL:'api/Weather/getWeather?',
+  getHistoryURL: 'api/Weather/getHistory?',
+  postURL: 'api/Weather/postToHistory'
 };
